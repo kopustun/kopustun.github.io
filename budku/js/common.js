d2d025c9e07451e7b55a5899901a7291fa7726b2
kopustun <kopustun@gmail.com>
2018-04-15 05:37:07 +0300

    desf

--- budku/js/common.js
+++ budku/js/common.js
@@ -2,4 +2,42 @@ $(function() {
 
 	// Custom JS
 
+	$('.buttonform').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+		fixedContentPos: false,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in',
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+
+//E-mail Ajax Send
+	$("form.callback").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+			setTimeout(function() {
+				$(th).find('.success').removeClass('active').fadeOut();
+				th.trigger("reset");
+			}, 3000);
+		});
+		return false;
+	});
 });
